ca4c69634075c1907889a14c741548bf1746afac
Aboltys754 <warwar754@mail.ru>
2023-12-19 12:01:03 +0500

    create crud

--- app/crud.py
+++ app/crud.py
@@ -3,3 +3,30 @@ from sqlalchemy.orm import Session
 from . import models, schemas
 
 
+def createShop(db: Session, shop: schemas.CreateShop):
+    """Создать магазин"""
+    db_shop = models.Shop(number_shop=shop.number_shop, address_shop=shop.address_shop)
+    db.add(db_shop)
+    db.commit()
+    db.refresh(db_shop)
+    return db_shop
+
+
+def getShops(db: Session):
+    """Получить все  магазины"""
+    return db.query(models.Shop).all()
+
+
+def createEmployee(db: Session, employee: schemas.CreateEmployee):
+    """Создать сотрудника"""
+    db_employee = models.Employee(name_employee=employee.name_employee,
+                                  age_employee=employee.age_employee,
+                                  post_employee=employee.post_employee)
+    db.add(db_employee)
+    db.commit()
+    db.refresh(db_employee)
+    return db_employee
+
+
+def getEmployees(db: Session):
+    return db.query(models.Employee).all()
